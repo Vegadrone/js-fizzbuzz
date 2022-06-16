26860881e6c5e0a39fc3fe1462a51d13550c5c10
Vegadrone <spenaantonino@gmail.com>
2022-06-16 15:23:00 +0200

    Verbalize problem

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,20 @@
-console.log('Inizio l\'esercizio!')
+/*
+Per fare questo esercizio ho bisogno di:
+    1 - Inizializzare un ciclo for: for (let i = 1; i <= 100; i++) {};
+    2 - Capire come targettare i numeri divisibili per 3 e che lascino resto 0 (attraverso %);
+    3 - Capire come targettare i numeri divisibili per 5 e che lascino resto 0 (attraverso %);
+    3 - Capire come targettare i numeri divisibili per 3 e 5 (usando un if statement con operatore && AND
+        che permette di verificare la condizione solo se tutte e due sono vere) 
+        che lascino resto 0 (attraverso %);
+A questo punto attraverso if statement stampare prima in console e poi (se riesci in tempo) nel DOM ==>(attraverso
+document.createElement ('nome tag') append e innerHTML.):
+    - Fizz al posto dei numeri divisibili per 3 e che danno resto 0;
+    - Buzz al posto dei numeri divisibili per 5 e che danno resto 0;
+    - FizzBuzz al posto dei numeri divisibili per 3 e per 5 e che danno resto 0;
+
+
+
+
+
+
+*/
